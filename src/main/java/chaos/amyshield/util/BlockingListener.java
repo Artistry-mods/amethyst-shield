6b27252cc81057b2b0e7a66d0b588c86b7a96459
HerrChaos <cedrik.zwick@gmx.net>
2024-05-20 17:24:34 +0200

    fixed a few bugs and made crafting items obtainable

--- src/main/java/chaos/amyshield/util/BlockingListener.java
+++ src/main/java/chaos/amyshield/util/BlockingListener.java
@@ -1,10 +1,12 @@
 package chaos.amyshield.util;
 
+import chaos.amyshield.AmethystShield;
 import chaos.amyshield.Item.ModItems;
 import chaos.amyshield.Item.custom.AmethystShieldItem;
 import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldBlockCallback;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
@@ -19,8 +21,9 @@ public class BlockingListener {
     }
 
     private static ActionResult listener(LivingEntity defender, DamageSource damageSource, float amount, Hand hand, ItemStack itemStack) {
-        if (!defender.isInvulnerableTo(damageSource) && defender.canTakeDamage() && itemStack.getItem() == ModItems.AMETHYST_SHIELD && defender instanceof ServerPlayerEntity) {
-            AmethystShieldItem.addCharge(((IEntityDataSaver) defender), amount);
+        if (!defender.isInvulnerableTo(damageSource) && defender.canTakeDamage() && itemStack.getItem() == ModItems.AMETHYST_SHIELD && defender instanceof ServerPlayerEntity && !damageSource.isOf(DamageTypes.MAGIC)) {
+            double addedCharge = amount * AmethystShield.BLOCK_GAIN_MULTIPLIER;
+            AmethystShieldItem.addCharge(((IEntityDataSaver) defender), (float) addedCharge);
             syncCharge(AmethystShieldItem.getCharge(((IEntityDataSaver) defender)), (ServerPlayerEntity) defender);
         }
         return ActionResult.PASS;
