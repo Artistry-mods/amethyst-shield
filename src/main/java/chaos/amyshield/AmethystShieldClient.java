8fe54be095315c9ef33a9cd60900514ed5f0d48d
HerrChaos <138036657+HerrChaos@users.noreply.github.com>
2025-02-13 20:00:35 +0100

    - made the shield lest annoying. (Optional resource pack). Thanks to... uhh "ꖌ∷╎ᒲᓭ||||"!! - cleaned up some useless folders

--- src/main/java/chaos/amyshield/AmethystShieldClient.java
+++ src/main/java/chaos/amyshield/AmethystShieldClient.java
@@ -6,6 +6,13 @@ import chaos.amyshield.particles.client.ModClientParticles;
 import chaos.amyshield.ui.client.ChargeHudOverlay;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.Objects;
 
 public class AmethystShieldClient implements ClientModInitializer {
 	@Override
@@ -14,6 +21,12 @@ public class AmethystShieldClient implements ClientModInitializer {
 		ModPackets.registerGlobalReceiversS2C();
 		ModEntityModels.registerModEntityModels();
 		HudRenderCallback.EVENT.register(new ChargeHudOverlay());
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        ResourceManagerHelper.registerBuiltinResourcePack(
+			Objects.requireNonNull(Identifier.of(AmethystShield.MOD_ID, "lower_amethyst_shield")),
+            FabricLoader.getInstance().getModContainer(AmethystShield.MOD_ID).orElseThrow(),
+            Text.of("Lower Amethyst Shield"),
+            ResourcePackActivationType.NORMAL
+        );
 	}
 }
